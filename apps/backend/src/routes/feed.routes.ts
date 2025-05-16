bf2d77c5966c5653a528799744179f16eb7037e8
Alexin <alexindevs@gmail.com>
2025-05-16 15:32:42 +0100

    fix: allow businesses to view feed data

--- apps/backend/src/routes/feed.routes.ts
+++ apps/backend/src/routes/feed.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { FeedController } from '../controllers/feed.controller';
 import { requireAuth } from '../middlewares/auth.middleware';
-import { authorizeRole } from '../middlewares/role.middleware';
-import { Role } from '../../generated/prisma';
+// import { authorizeRole } from '../middlewares/role.middleware';
+// import { Role } from '../../generated/prisma';
 
 const router = Router();
 const controller = new FeedController();
@@ -10,13 +10,11 @@ const controller = new FeedController();
 router.get(
   '/post/:contentId',
   requireAuth,
-  authorizeRole([Role.MEMBER]),
   controller.getContentDetails
 );
 router.get(
   '/:businessSlug',
   requireAuth,
-  authorizeRole([Role.MEMBER]),
   controller.getFeed
 );
 
